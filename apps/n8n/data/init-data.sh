77d60e371a1ab85e126fcd3c9426a5cd13eb0a22
lerrrii <lerri5074@gmail.com>
2025-06-05 22:34:02 +0200

    Update init-data.sh

--- apps/n8n/data/init-data.sh
+++ apps/n8n/data/init-data.sh
@@ -2,7 +2,6 @@
 set -e;
 
 # Create the extension as the superuser (POSTGRES_USER)
-# This command will be run by the 'tipi' user in the 'n8n' database.
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 EOSQL
@@ -14,9 +13,15 @@ if [ -n "${POSTGRES_NON_ROOT_USER:-}" ] && [ -n "${POSTGRES_NON_ROOT_PASSWORD:-}
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 		CREATE USER ${POSTGRES_NON_ROOT_USER} WITH PASSWORD '${POSTGRES_NON_ROOT_PASSWORD}';
 		GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_NON_ROOT_USER};
-		-- It's also good practice to grant usage on the public schema explicitly,
+
+		-- Grant CREATE and USAGE on the public schema to the non-root user
+		-- This allows the user to create tables in the public schema.
+		GRANT CREATE, USAGE ON SCHEMA public TO ${POSTGRES_NON_ROOT_USER};
+
+		-- It's also good practice to grant usage on the public schema explicitly, (This line is now covered by the line above)
 		-- where extensions like uuid-ossp typically install their functions.
-		GRANT USAGE ON SCHEMA public TO ${POSTGRES_NON_ROOT_USER};
+		-- GRANT USAGE ON SCHEMA public TO ${POSTGRES_NON_ROOT_USER}; -- Redundant if CREATE, USAGE is granted above
+
 		GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${POSTGRES_NON_ROOT_USER}; -- If n8n needs to read pg_catalog or other system tables in public
 		GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO ${POSTGRES_NON_ROOT_USER}; -- To use functions from the extension
 	EOSQL
